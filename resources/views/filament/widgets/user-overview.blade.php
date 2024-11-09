029a350d2f94553fa74e7400859ae3dc319e9290
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-11-09 20:44:15 +0330

    ref useroverview

--- resources/views/filament/widgets/user-overview.blade.php
+++ resources/views/filament/widgets/user-overview.blade.php
@@ -2,6 +2,5 @@
     <x-filament::card>
         <h2 class="text-xl font-bold">User Overview</h2>
         <p>Total Users: {{ $totalUsers }}</p>
-        <x-tabler-alert-circle />
     </x-filament::card>
 </x-filament::widget>
