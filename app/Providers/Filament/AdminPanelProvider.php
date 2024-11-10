82e4479ff6a485e42e1d70b29172679b22669d5f
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-11-10 12:57:20 +0330

    feat (provider) : add path to filament provider

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -27,6 +27,11 @@ class AdminPanelProvider extends PanelProvider
     {
         return $panel
             ->default()
+
+            ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
+            ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
+            ->discoverClusters(in: app_path('Filament/Clusters'), for: 'App\\Filament\\Clusters')
+
             ->id('admin')
             ->path('admin')
             ->login()
