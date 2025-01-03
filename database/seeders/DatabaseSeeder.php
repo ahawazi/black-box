616823098152131878c0e36b18c3194e956dece5
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2025-01-03 11:59:17 +0330

    refactor: comment out SuperAdminSeeder in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -9,7 +9,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            SuperAdminSeeder::class,
+            // SuperAdminSeeder::class,
             //ShieldSeeder::class,
             CategorySeeder::class,
             ProductSeeder::class,
